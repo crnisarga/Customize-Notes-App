78cc2fe43f484e562646c5c5f5141234c1637b54
Nisarga <nisargagowdacr@gmail.com>
2024-10-20 18:15:36 +0530

    checking in github

--- app/src/main/java/com/example/notesapp/fragment/saveOrDeleteFragment.kt
+++ app/src/main/java/com/example/notesapp/fragment/saveOrDeleteFragment.kt
@@ -88,6 +88,6 @@ class saveOrDeleteFragment : Fragment(R.layout.fragment_save_or_delete) {
     }
 
     private fun saveNote() {
-        TODO("Not yet implemented")
+        TODO("Not yet implemented the tested")
     }
 }
